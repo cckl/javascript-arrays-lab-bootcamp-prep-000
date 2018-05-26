ac723c4bae84311f5a33c4feebb9ef133281b881
Christina L <clingc7@gmail.com>
2018-05-26 07:46:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  let cats = [...kittens, "Broom"]
+  return cats
+}
+
+function prependKitten(name) {
+  let cats = ["Arnold", ...kittens]
+  return cats
+}
+
+function removeLastKitten() {
+  let cats = kittens.splice(2, 1)
+  return cats
+}
